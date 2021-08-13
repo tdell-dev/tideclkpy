7dc87a685bcb17b6b854c326ea3a745505cb67cf
Uiblr <uiblr@github.com>
2021-08-13 02:31:16 -0700

    Successfully issuing first request and bringup on boot

--- tideclk/tideclk/console.py
+++ tideclk/tideclk/console.py
@@ -1,12 +1,34 @@
+import time
+import logging
+from systemd.journal import JournaldLogHandler
+
+#internal classes
 from . import requester
 from . import display
 
-def main():
-    try:
-        display.init()
-        display.run()
-    except KeyboardInterrupt:
-        print("Exiting")
-#    except:
-#        print("Exiting due to Exception")
+#set up logging
+logger = logging.getLogger(__name__)
+journald_handler=JournaldLogHandler()
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+logger.addHandler(journald_handler)
+logger.setLevel(logging.DEBUG)
 
+def main():
+    logger.info('TideClk Started!')
+    retry=0
+    while retry ==0:
+        logger.info('Attempting request')
+        try:
+            display.init()
+            req = requester.Requester(log=logger)
+            display.run()
+            retry =0
+  
+        except KeyboardInterrupt:
+            logger.info("Exiting due to Keyboard Interrupt")
+        except:
+            logger.info("Retrying due to Exception")
+            time.sleep(1)
+            retry = 1
